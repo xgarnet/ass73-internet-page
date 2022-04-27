8edd643155c0b708d5a01672b5d4f91d60848118
xgarnet <vt7970170@gmail.com>
2022-04-27 17:18:17 +0200

    add some components, ad link with page navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SupportComponent } from './support/support.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
+import { OrganisationComponent } from './organisation/organisation.component';
+import { DownloadsComponent } from './downloads/downloads.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+import { AgbComponent } from './agb/agb.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    SupportComponent,
+    PageNotFoundComponent,
+    OrganisationComponent,
+    DownloadsComponent,
+    NewsComponent,
+    ContactComponent,
+    AgbComponent,
+    ImpressumComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'organisation', component: OrganisationComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: 'customers', component: CustomersComponent},
+      {path: 'downloads', component: DownloadsComponent},
+      {path: 'news', component: NewsComponent},
+      {path: 'support', component: SupportComponent},
+      {path: 'contact', component: ContactComponent},
+      {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
